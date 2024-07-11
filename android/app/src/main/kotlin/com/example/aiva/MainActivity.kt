1198fa86ac92c9518fd49211d23898bad69b8b46
shagun <shagun2001ch@gmail.com>
2024-07-11 15:22:26 +0530

    added files

---
+++ android/app/src/main/kotlin/com/example/aiva/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aiva
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:aiva/helper/pref.dart';
+import 'package:aiva/screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Aiva",
+      debugShowCheckedModeBanner: false,
+
+      themeMode: Pref.defaultTheme,
+
+      //dark
+      darkTheme: ThemeData(
+          useMaterial3: false,
+          brightness: Brightness.dark,
+          appBarTheme: const AppBarTheme(
+            elevation: 1,
+            centerTitle: true,
+            titleTextStyle:
+                TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          )),
+
+      //light
+      theme: ThemeData(
+          useMaterial3: false,
+          appBarTheme: const AppBarTheme(
+            elevation: 1,
+            centerTitle: true,
+            backgroundColor: Colors.white,
+            iconTheme: IconThemeData(color: Colors.blue),
+            titleTextStyle: TextStyle(
+                color: Colors.blue, fontSize: 20, fontWeight: FontWeight.w500),
+          )),
+
+      //
+      home: const SplashScreen(),
+    );
+  }
+}
+
+extension AppTheme on ThemeData {
+  //light text color
+  Color get lightTextColor =>
+      brightness == Brightness.dark ? Colors.white70 : Colors.black54;
+
+  //button color
+  Color get buttonColor =>
+      brightness == Brightness.dark ? Colors.cyan.withOpacity(.5) : Colors.blue;
+}
